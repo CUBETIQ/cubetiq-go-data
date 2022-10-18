2dceae1b4b916b4d02918e18a374a3eef479716b
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-10-18 15:36:48 +0700

    add page model

---
+++ model/page/page.go
@@ -0,0 +1,8 @@
+package page
+
+type Page struct {
+	TotalPage  int64 `json:"total_page"`
+	Page       int64 `json:"page"`
+	TotalCount int64 `json:"total_count"`
+	PageSize   int64 `json:"pageSize"`
+}
--- model/response/body/body.go
+++ model/response/body/body.go
@@ -1,10 +1,22 @@
 package body
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/cubetiq/cubetiq-data-go/model/page"
+	"github.com/gofiber/fiber/v2"
+)
 
 func BaseBodyResponse(data interface{}, status int16, msg string) *fiber.Map {
 	return &fiber.Map{
 		"data":   data,
+		"status": status,
+		"msg":    msg,
+	}
+}
+
+func BasePageBodyResponse(data interface{}, page page.Page, status int16, msg string) *fiber.Map {
+	return &fiber.Map{
+		"data":   data,
+		"page":   page,
 		"status": status,
 		"msg":    msg,
 	}
